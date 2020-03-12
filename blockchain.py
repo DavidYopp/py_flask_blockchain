df881461793094228369b32a1d3254bf6e580857
Josh Yopp <davidjyopp@gmail.com>
2020-03-12 15:33:43 -0400

    Added Genesis block generation added a last_block function to return the last block in the chain Fleshed out the new block and has functions as well

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,36 @@
-
+import hashlib
+import json
+from time import time
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # create a new block and add it to the chain
-        pass
+        # Generate a genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in the Blockchain
+        :param proof: <int> Thr proof given by the proof of work algorithm
+        :param previous_hash: (Optional) <str> Has of previous block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        #Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         # adds a new transaction to the list of transactions
@@ -27,10 +50,21 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
     @staticmethod
     def hash(block):
-        # hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        #The dictionary must be ordered or we will have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
